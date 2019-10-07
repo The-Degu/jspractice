dfe964fce62a7b371478fa54162f32c64ec7306a
The-Degu <rpruit1@artic.edu>
2019-10-07 15:59:37 -0500

    update

--- js.js
+++ js.js
@@ -1,5 +1,14 @@
-for ( let i = 0 , i =< 5, i++ ) {
-    console.log ("name" + i)
+//1
+
+for( let i = 0; i <= 5; i++) {
+    console.log("Riely" + i)
 }
-let name = 'Riley'
-    
+
+// while( i = 0, i <= 10, i++) {
+//     text += "The number is " + i;
+//     i++;
+//   }
+//2
+//truthfully i have no idea where to evin start for the other two.  
+//like not evin an incaling of the first line to write
+
